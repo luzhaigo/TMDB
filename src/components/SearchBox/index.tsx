540125090615ef12c023d228f26f959c47b882fe
luzhaigo <gitluzhai@gmail.com>
2024-03-13 17:48:53 +0000

    feat(searchbox): users can click clear icon to remove input value

--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -1,4 +1,4 @@
-import { FC, FormEvent, useCallback, useRef, ElementRef } from 'react';
+import { ChangeEvent, FC, FormEvent, useCallback, useState } from 'react';
 import './searchBox.css';
 
 export type Props = {
@@ -6,20 +6,31 @@ export type Props = {
 };
 
 const SearchBox: FC<Props> = ({ onSearch }) => {
-  const inputRef = useRef<ElementRef<'input'>>(null);
+  const [search, setSearch] = useState<string>();
+
+  const clearInput = useCallback(() => {
+    setSearch('');
+    onSearch?.();
+  }, [onSearch]);
+
+  const onInputChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => setSearch(e.target.value),
+    [],
+  );
+
   const submit = useCallback(
     (e: FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-      if (!inputRef.current) return;
-      onSearch?.(inputRef.current.value);
+
+      onSearch?.(search);
     },
-    [onSearch],
+    [search, onSearch],
   );
 
   return (
     <form className="searchBox" autoComplete="off" onSubmit={submit}>
       <svg
-        className="searchBox__icon"
+        className="searchBox__searchIcon"
         xmlns="http://www.w3.org/2000/svg"
         width="16"
         height="16"
@@ -28,11 +39,24 @@ const SearchBox: FC<Props> = ({ onSearch }) => {
         <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
       </svg>
       <input
-        ref={inputRef}
+        value={search}
         className="searchBox__input"
         name="search"
         placeholder="Search"
+        onChange={onInputChange}
       />
+      {search && (
+        <svg
+          className="searchBox__xMarkIcon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 512 512"
+          onClick={clearInput}
+        >
+          <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z" />
+        </svg>
+      )}
     </form>
   );
 };
