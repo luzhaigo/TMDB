78e04924cc0c592dbb1f80304560e3713500ad44
luzhaigo <gitluzhai@gmail.com>
2024-03-13 20:32:43 +0000

    build: add git hash in the head tag

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { execSync } from 'child_process';
+
+const commitHash = execSync('git rev-parse HEAD').toString().trim();
+
+const htmlPlugin = () => {
+  return {
+    name: 'html-transform',
+    transformIndexHtml(html: string) {
+      return html.replace(/\{\{COMMIT_HASH\}\}/, commitHash);
+    },
+  };
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/TMDB/',
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), htmlPlugin()],
 });
